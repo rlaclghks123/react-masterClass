9f63c70295cd1a60af1d3185cc2bfbd0e5aaf913
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-13 19:25:53 +0900

    styled-component-2
    component확장 상속, override

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import styled from "styled-components";
 const Father = styled.div`
 display:flex;`;
 
-const BoxOne = styled.span`
-background-color:teal;
+const Box = styled.div`
+background-color:${(props) => props.bgColor};
 width:100px;
 height:100px;`
 
-const BoxTwo = styled.span`
-background-color:tomato;
-width:100px;
-height:100px;`
+const Circle = styled(Box)`
+border-radius:50%;`;
 
 
 function App() {
   return (
     <Father>
-      <BoxOne></BoxOne>
-      <BoxTwo></BoxTwo>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father >
   );
 }
