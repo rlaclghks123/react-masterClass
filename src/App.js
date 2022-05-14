15f4fa34dbf6638d9a0bb70c8124a3ddad1250f4
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-14 16:47:57 +0900

    styled-component-4
    Pseudo Selector

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
+
 import styled from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
 display:flex;`;
 
-const Input = styled.input.attrs({ required: true, minLength: 30 })
-  `background-color: teal`;
+const Emoji = styled.div`
+font-size: 50px;
+&:hover{
+  opacity: 0;
+}
+`;
+
+const Box = styled.div`
+width: 100px;
+height: 100px;
+background-color: teal;
+display: flex;
+justify-content: center;
+align-items: center;
+
+${Emoji}{
+  font-size: 10px;
+  cursor: pointer;
+}
+
+span{
+  font-size: 50px;
+  
+}
+`;
+
 
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji as="span">😊</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
