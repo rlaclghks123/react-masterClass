1c4db26034cb0e3c774688a9661827980fefb28e
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-15 14:31:46 +0900

    theme
    darkmode , lightmode

--- src/App.js
+++ src/App.js
@@ -2,45 +2,16 @@
 import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
-display:flex;`;
+display:flex;
+justify-content: center;
+align-items:center;
+width:100vh;
+height:100vh;
+background-color : ${(props) => props.theme.backgroundColor}`;
 
-const Emoji = styled.div`
-font-size: 50px;
-&:hover{
-  opacity: 0;
-}
-`;
-
-const rotation = keyframes`
-0%{
-  transform: rotate(0deg);
-}
-50%{
-  transform: rotate(360deg);
-}
-100%{
-  transform: rotate(0deg);
-}
-`
 
 const Box = styled.div`
-width: 100px;
-height: 100px;
-background-color: ${(props) => props.bgColor};
-display: flex;
-justify-content: center;
-align-items: center;
-animation: ${rotation} 5s linear infinite;
-
-${Emoji}{
-  font-size: 10px;
-  cursor: pointer;
-}
-
-span{
-  font-size: 50px;
-  
-}
+color:${(props) => props.theme.textColor};
 `;
 
 
@@ -48,9 +19,7 @@ span{
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="tomato">
-        <Emoji as="span">😊</Emoji>
-      </Box>
+      <Box>Hello</Box>
     </Wrapper>
   );
 }
