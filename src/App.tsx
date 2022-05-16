01e9bb847bd8de8241d10407844fca1dfa985857
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-16 19:12:30 +0900

    typescripts - prop2
    default props, optional props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
+      <Circle bgColor="teal" borderColor="tomato" text="Hello" />
       <Circle bgColor="tomato" />
     </div>
   );
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
     bgColor: string;
+    borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,13 +10,20 @@ width:100px;
 height:100px;
 border-radius:50%;
 background-color: ${(props) => props.bgColor};
+border: 1px solid ${(props) => props.borderColor};
 `;
 
+interface CircleProps {
+    bgColor: string;
+    borderColor?: string;
+    text?: string;
+}
 
-
-function Circle({ bgColor }: ContainerProps) {
+function Circle({ bgColor, borderColor, text = "default" }: CircleProps) {
     return (
-        <Container bgColor={bgColor} />
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
     )
 }
 
