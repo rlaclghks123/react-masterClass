6d0a2d63d6ac266c7c65a6f8ca41d3fe4037f4af
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-17 17:03:40 +0900

    CRYPTO TRACKER - set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-background-color:${(props) => props.theme.bgColor};
-display: flex;
-justify-content: center;
-align-items: center;
-width:100%;
-height:100vh;
-`;
 
-const H1 = styled.h1`
-color:${(props) => props.theme.textColor};
-`
 
 function App() {
   return (
-    <Container>
-      <H1>Hello</H1>
-    </Container>
+    <Router />
   );
 }
 
