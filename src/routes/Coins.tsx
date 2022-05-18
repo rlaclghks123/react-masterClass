1ddf67c0a7108cfe479e3668b86d0b8d2b9864ed
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-18 16:48:53 +0900

    CRYPTO TRACKER - home -part2
    api 불러오기

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -7,7 +8,11 @@ const Title = styled.h1`
     font-size:50px;
 `;
 
-const Container = styled.div``;
+const Container = styled.div`
+padding:20px;
+max-width: 480px;
+margin:0px auto; 
+`;
 
 const Header = styled.header`
 height:20vh;
@@ -33,47 +38,49 @@ a{
     }
 }
 `;
+const Loader = styled.div`
+    font-size: 40px;
+    display: flex;
+    justify-content: center;
+    `;
 
-const coins = [{
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-},
-{
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-},
-{
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-},];
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+};
+
+
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch("https://api.coinpaprika.com/v1/coins");
+                const json = await response.json();
+                setCoins(json.slice(0, 100));
+                setLoading(false);
+            }
+        )();
+    }, []);
     return (
         <Container>
             <Header>
                 <Title> 코인</ Title>
             </Header>
-            <CoinList>
+            {loading ? <Loader>Loading...</Loader> : <CoinList>
                 {coins.map((coin) =>
                     <Coin key={coin.id}>
                         <Link to={`/${coin.id}`}>{coin.name} &rarr; </Link>
                     </Coin>)}
-            </CoinList>
+            </CoinList>}
         </Container>
     );
 }
