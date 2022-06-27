eb287060983f8229003229f59e65ec45624a6da0
Chi Hwan Kim <rlaclghks123@naver.com>
2022-06-27 22:49:26 +0900

    apexChart Data 가져오기.
    이때 주의할점은 ?? []를 통해 number 배열이라는걸 강제해줘야 한다. 이유는 undefined 될 경우가 있기 때문이다.

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,13 +1,35 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
     coinId: string;
 }
 
+interface IHistorycal {
+    time_open: string,
+    time_close: string,
+    open: number,
+    high: number,
+    low: number,
+    close: number,
+    volume: number,
+    market_cap: number,
+}
+
+
 function Chart({ coinId }: ChartProps) {
-    const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-    return <h1>Chart</h1>
+    const { isLoading, data } = useQuery<IHistorycal[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+
+    return <div>{isLoading ? ("Loading...") : (<ApexChart type="line"
+        series={[
+            {
+                name: "Price",
+                data: data?.map((price) => price.close) ?? [],
+            },
+        ]}
+
+    />)}</div>
 }
 
 export default Chart
